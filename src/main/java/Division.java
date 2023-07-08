b558a2a54d0ee40256592c18042b4aab549ade5f
Alexandr Tsvilikhovsky <alexandr.tsvilikhovsky@softermii.com>
2023-07-08 09:34:29 +0300

    Division Method

--- src/main/java/Division.java
+++ src/main/java/Division.java
@@ -1,5 +1,5 @@
-public class Division{
-    public static double divide(int a, int b){
-        return a * Math.pow(b,-1);
+public class Division {
+    public static double divide(int a, int b) {
+        return a * Math.pow(b, -1);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Division.divide(1,3);
-        Division.divide(2,3);
-        Division.divide(-1,0);
-        Division.divide(1,-3);
+        Division.divide(1, 3);
+        Division.divide(2, 3);
+        Division.divide(-1, 0);
+        Division.divide(1, -3);
     }
 }
